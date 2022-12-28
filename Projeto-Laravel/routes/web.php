d72c89038e0d3d0aed8ca094c22eb2880e4c2d0f
Mauricio <mauricio@casadaweb.net>
2022-12-28 07:37:27 -0300

    Atualização 1.0.0

--- Projeto-Laravel/routes/web.php
+++ Projeto-Laravel/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,55 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//LINKAR UM CONTROLLER
+Route::get('/user/{user}', [UserController::class, 'show']);
+
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+
+
+
+/*
+//USANDO REQUEST
+Route::get('/request', function (Request $request) {
+    //$r = $request->all();
+    
+    //Verifica se o Campo é Preenchido
+    $r = $request->whenFilled('keyword', function($input) {
+        dd('x', $input);
+    });
+
+
+    //dd($request);
+    return 'x';
 });
+
+Route::get('/user/{user}', function (User $user) {
+    dd($user);
+    return $user;
+})->name('user');
+
+//GRUPO DE ROTAS
+Route::prefix('usuarios')->group(function() {
+
+Route::get('', function() {
+    return 'usuário';
+})->name('usuarios');
+
+Route::get('/{id}', function() {
+    return 'Mostrar Detalhes';
+})->name('usuarios.show');
+
+Route::get('/{id}/tags', function() {
+    return 'Tags do Usuário';
+})->name('usuarios.tags');
+
+});
+
+//ABRIR NOME DA PÁGINA SEM A /
+Route::get('/empresa/{string?}', function ($string = null) {
+    return $string;
+    //return view('welcome');
+})->name('empresa');
+*/
